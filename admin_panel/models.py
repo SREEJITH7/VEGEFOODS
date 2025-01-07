e3b950cbc50988c2d49e069336b4194a7a0a5da3
SREEJITH7 <karnjaps4@gmail.com>
2025-01-08 01:22:59 +0530

    timezone replaced imported in admin models

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.db import models
 from django.core.validators import MinValueValidator
+from django.utils import timezone
 # -----------------------------------------------------
 
 class Offer(models.Model):
